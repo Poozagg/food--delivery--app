b36b03647e0d82a4e971597726beb14c88e1df6a
Pooja Gurung <pooja_grg07@hotmail.com>
2024-05-06 11:13:02 +0100

    auth0 installed and initialized

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import './global.css'
 import { BrowserRouter as Router } from "react-router-dom"
 import AppRoutes from './AppRoutes'
+import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/*  use router instead of app to keep code cleaner & more organised*/}
     <Router>
-      <AppRoutes />
+      <Auth0ProviderWithNavigate>
+        <AppRoutes />
+      </Auth0ProviderWithNavigate>
     </Router>
   </React.StrictMode>,
 )
