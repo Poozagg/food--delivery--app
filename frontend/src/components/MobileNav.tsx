3115e457ec20ad859fdc0e5917984fbd6676f619
Pooja Gurung <pooja_grg07@hotmail.com>
2024-05-06 17:19:39 +0100

    mobileNav login connected with auth0 login

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -2,8 +2,10 @@ import  {Menu} from 'lucide-react'
 import { Sheet, SheetTrigger, SheetTitle, SheetContent, SheetDescription } from './ui/sheet'
 import {Separator} from './ui/separator'
 import {Button} from './ui/button'
+import {useAuth0} from '@auth0/auth0-react'
 
 export default function MobileNav() {
+  const { loginWithRedirect } = useAuth0();
   return (
     <Sheet>
       <SheetTrigger>
@@ -14,7 +16,11 @@ export default function MobileNav() {
         <SheetTitle>Welcome to mernEats.com! </SheetTitle>
         <Separator className="my-4"/>
         <SheetDescription className="flex">
-          <Button className="flex-1 font-bold bg-orange-600"> Log In </Button>
+          <Button
+          className="flex-1 font-bold bg-orange-600"
+          onClick={async () => await loginWithRedirect()}
+
+          > Log In </Button>
         </SheetDescription>
       </SheetContent>
 </Sheet>
